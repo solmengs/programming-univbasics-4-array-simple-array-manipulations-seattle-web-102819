fb59ef50541c9b4d91e5cdc4db881bcbf9b49bb9
mengistiab woldearegay <solmengs@gmail.com>
2019-10-17 03:43:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,3 +4,9 @@ artists.push(next_artist)
 
 artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
 artists.pop
+
+artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.unshift ="mengs"
+
+artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.shift
