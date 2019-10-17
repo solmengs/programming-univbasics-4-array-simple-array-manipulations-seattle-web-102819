db9a57428aff3ab2ed4927699c838a5524fca52f
mengistiab woldearegay <solmengs@gmail.com>
2019-10-17 03:47:28 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,12 @@
+artists = ["Hozier", "Ariana Grande", "Usher"]
+next_artist = "Beyonce"
+artists.push(next_artist)
+
+artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.pop
+
+artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.unshift ="mengs"
+
+artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.shift
